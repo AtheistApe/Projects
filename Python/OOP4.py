1751d42f700da3094817881e411244350f8bdd02
Gary Church <simiusmathematicus@gmail.com>
2016-10-29 20:07:53 -0700

    Added 'Manager' subclass of the 'Employee' class

--- Python/OOP4.py
+++ Python/OOP4.py
@@ -37,6 +37,8 @@ class Employee:
             return False
         return True
 
+emp1 = Employee('Jay', 'Lehmann', 60000)
+
 # When we define a subclass, we put the name of the parent class as the
 # argument to the subclass in the defintion of the subclass as the example
 # below shows
@@ -82,3 +84,65 @@ print(dev1.fullname())
 print(dev1.pay)
 dev1.apply_raise()
 print(dev1.pay)
+
+# To 'initiate' a subclass with extra information not contained in the
+# initialization method of the parent class, we have to provide the subclass
+# with its own initialization method. For example, suppose for the 'Developer'
+# class we also wish to pass in a 'programming language' attribute to describe
+# the developers main programming language.
+
+class Developer(Employee):
+    raise_amount = 1.10
+
+    def __init__(self, first, last, pay, prog_lang):
+        # We will use the parent class's '__init__' to initialize the
+        # 'first', 'last' and 'pay' attributes and the 'Developer' class's
+        # '__init__' method will initialize the 'prog_lang' attribute. We
+        # can call the parent's '__init__' method with either:
+        #
+        # 'super().__init__(first, last, pay)' or
+        # 'Employee.__init__(self, first, last, pay)'
+        #
+        # The fist call can be used if our subclass inherits from only one
+        # parent class (single inheritance) but we have to use the second
+        # call if the subclass inherits from more than one parent (multiple
+        # inheritance)
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
+
+dev1 = Developer('Gary', 'Church', 120000, 'Python')
+dev2 = Developer('Connor', 'Church', 115000, 'Java-script')
+print(dev1.email)
+print(dev1.prog_lang)
+
+# Let's now create another subclass ('Manager') of the 'Employee' class.
+# Instead of passing in a 'prog_lang' attribute, we will pass in a list of
+# employee objects which are under the managers control. Note: in the
+# '__init__' method we set the default argument of the 'employee' attribute
+# to 'None' rather than '[]' (an empty list). This was done because you should
+# never pass mutable data types as default arguments.
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def add_emp(self, emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+
+    def remove_emp(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+    def print_emps(self):
+        for emp in self.employees:
+            print('-->', emp.fullname())
+
+man1 = Manager('Aaryn', 'Church', 150000, [emp1, dev1])
+print(man1.email)
+man1.print_emps()
+man1.add_emp(dev2)
+man1.print_emps()
