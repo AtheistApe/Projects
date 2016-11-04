71c6489eeb0deda4fd2a23eb01e3794ea73127ec
AtheistApe <simiusmathematicus@gmail.com>
2016-11-04 08:17:13 -0700

    Added bullets for the player and code to detect collisions between the mob and player and between the bullets and mobs

--- Pygame/pygame_shmup.py
+++ Pygame/pygame_shmup.py
@@ -53,6 +53,10 @@ class Player(pygame.sprite.Sprite):
             self.rect.right = WIDTH
         if self.rect.left < 0:
             self.rect.left = 0
+    def shoot(self):
+        bullet = Bullet(self.rect.centerx, self.rect.top)
+        all_sprites.add(bullet)
+        bullets.add(bullet)
 
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
@@ -73,9 +77,26 @@ class Mob(pygame.sprite.Sprite):
             self.rect.y = random.randrange(-100, - 40)
             self.speedy = random.randrange(1, 8)
 
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((10, 20))
+        self.image.fill(YELLOW)
+        self.rect = self.image.get_rect()
+        self.rect.bottom = y
+        self.rect.centerx = x
+        self.speedy = -10
+
+    def update(self):
+        self.rect.y += self.speedy
+        # Kill if it moves off the top of the screen
+        if self.rect.bottom < 0:
+            self.kill() # Removes sprite.
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
+bullets = pygame.sprite.Group()
+
 player = Player()
 all_sprites.add(player)
 for i in range(8):
@@ -93,9 +114,29 @@ while running:
         # Check for closing the window
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player.shoot()
 
     # Update
     all_sprites.update() # Update all sprites in the sprite group
+
+    # Check to see if a bullet hit a mob. The function 'groupcollide'
+    # checks for collisions between objects in two groups of objects.
+    # The 'True' parameter removes the object from their respective
+    # groups during a collision between those objects.
+    hits = pygame.sprite.groupcollide(mobs, bullets, True, True)
+    for hit in hits:
+        m = Mob()
+        all_sprites.add(m)
+        mobs.add(m)
+        
+    # Check to see if a mob hit the player. The 'spritecollide'
+    # function returns a list of any of the mobs that hit the
+    # player.
+    hits = pygame.sprite.spritecollide(player, mobs, False)
+    if hits:
+        running = False
 
     # Draw / render
     screen.fill(BLACK)
