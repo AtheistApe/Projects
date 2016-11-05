4697d81d314bb94e7ef1faa79b83172bf3cc0e59
AtheistApe <simiusmathematicus@gmail.com>
2016-11-05 09:24:27 -0700

    Added graphics for the player, mob and bullet

--- Pygame/pygame_shmup.py
+++ Pygame/pygame_shmup.py
@@ -5,6 +5,9 @@
 import pygame
 import random
 import os
+# from os import path
+
+img_dir = os.path.join(os.path.dirname(__file__), 'img')
 
 WIDTH = 480
 HEIGHT = 600
@@ -34,8 +37,10 @@ clock = pygame.time.Clock()
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 40))
-        self.image.fill(GREEN)
+        # self.image = pygame.Surface((50, 40))
+        # self.image.fill(GREEN)
+        self.image = pygame.transform.scale(player_img, (50,38))
+        self.image.set_colorkey(BLACK) # Makes 'BLACK' transparent
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH/2
         self.rect.bottom = HEIGHT - 10
@@ -61,8 +66,10 @@ class Player(pygame.sprite.Sprite):
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((30, 40))
-        self.image.fill(RED)
+        # self.image = pygame.Surface((30, 40))
+        # self.image.fill(RED)
+        self.image = meteor_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(WIDTH - self.rect.width)
         self.rect.y = random.randrange(-100, - 40)
@@ -80,8 +87,10 @@ class Mob(pygame.sprite.Sprite):
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((10, 20))
-        self.image.fill(YELLOW)
+        # self.image = pygame.Surface((10, 20))
+        # self.image.fill(YELLOW)
+        self.image = bullet_img
+        self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
         self.rect.bottom = y
         self.rect.centerx = x
@@ -92,6 +101,13 @@ class Bullet(pygame.sprite.Sprite):
         # Kill if it moves off the top of the screen
         if self.rect.bottom < 0:
             self.kill() # Removes sprite.
+
+# Load all game graphics.
+background = pygame.image.load(os.path.join(img_dir, "starfield.png")).convert()
+background_rect = background.get_rect()
+player_img = pygame.image.load(os.path.join(img_dir, "playerShip1_orange.png")).convert()
+meteor_img = pygame.image.load(os.path.join(img_dir, "meteorBrown_med1.png")).convert()
+bullet_img = pygame.image.load(os.path.join(img_dir, "LaserRed16.png")).convert()
 
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
@@ -140,6 +156,7 @@ while running:
 
     # Draw / render
     screen.fill(BLACK)
+    screen.blit(background, background_rect)
     all_sprites.draw(screen) # Draw all sprites in the sprite group
 
     # *after* drawing everything, flip the display
