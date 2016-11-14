7b627c7e56aca38f5de4c4eab184f7672a551103
AtheistApe <simiusmathematicus@gmail.com>
2016-11-13 21:21:27 -0800

    Reorganized code in 'hello_world.c'

--- Build_Lisp/hello_world.c
+++ Build_Lisp/hello_world.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
+int hello_world_1(int n) {
+  for (int i = 1; i <= n; i++){
+    puts("Hello World!");
+  }
+  return 0;
+}
+
+int hello_world_2(int n){
+  int k = 1;
+  while (k <= n){
+    puts("Hello World!");
+    k++;
+  }
+  return 0;
+}
+
 int main(int argc, char** argv) {
-
-  int hello_world_1(int n) {
-    for (int i = 1; i <= n; i++){
-      puts("Hello World!");
-    }
-    return 0;
-  }
-
-  int hello_world_2(int n){
-    int k = 1;
-    while (k <= n){
-      puts("Hello World!");
-      k++;
-    }
-    return 0;
-  }
 
   hello_world_1(3);
   puts("");
