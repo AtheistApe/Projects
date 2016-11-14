10165013f87756cad0e8707d3224ee55bd1a774b
AtheistApe <simiusmathematicus@gmail.com>
2016-11-13 16:32:12 -0800

    Extended the 'hello_world.c' file and added a 'mycc.bat' file to set parameters for compiling *.c files'

--- Build_Lisp/hello_world.c
+++ Build_Lisp/hello_world.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-  puts("Hello Gary! How are you");
-  return 0;
+
+  int hello_world_1(int n) {
+    for (int i = 1; i <= n; i++){
+      puts("Hello World!");
+    }
+    return 0;
+  }
+
+  int hello_world_2(int n){
+    int k = 1;
+    while (k <= n){
+      puts("Hello World!");
+      k++;
+    }
+    return 0;
+  }
+
+  hello_world_1(3);
+  puts("");
+  hello_world_2(4);
 }
---
+++ Build_Lisp/mycc.bat
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE=$1
+
+if [[ $FILE =~ (.+)\.c ]]
+then
+  FILE_NAME=${BASH_REMATCH[1]}
+  # echo $FILE_NAME
+else
+  echo "$FILE is NOT a c file"
+  exit 1
+fi
+
+cc -std=c99 -Wall -Wstrict-prototypes -ansi -pedantic -g $FILE -o $FILE_NAME
