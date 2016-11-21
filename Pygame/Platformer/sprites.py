874710c96465477583d6913d9aa4662da0cb61a3
AtheistApe <simiusmathematicus@gmail.com>
2016-11-21 08:30:47 -0800

    Added a 'Platform' class to allow for platforms in the game. Made other small modifications to the code

--- Pygame/Platformer/sprites.py
+++ Pygame/Platformer/sprites.py
@@ -9,13 +9,14 @@ class Player(pg.sprite.Sprite):
         self.image = pg.Surface((30, 40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH/2, HEIGHT/2)
         self.pos = vec(WIDTH/2, HEIGHT/2)
+        # self.rect.center = self.pos
+        self.rect.midbottom = self.pos
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
 
     def update(self):
-        self.acc = vec(0, 0)
+        self.acc = vec(0, 0.25)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
             self.acc.x = -1*PLAYER_ACC
@@ -23,7 +24,7 @@ class Player(pg.sprite.Sprite):
             self.acc.x = PLAYER_ACC
 
         # Apply friction
-        self.acc += self.vel * PLAYER_FRICTION
+        self.acc.x += self.vel.x * PLAYER_FRICTION
         # Equations of motion
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
@@ -33,4 +34,13 @@ class Player(pg.sprite.Sprite):
         if self.pos.x < 0:
             self.pos.x = WIDTH
 
-        self.rect.center = self.pos
+        self.rect.midbottom = self.pos
+
+class Platform(pg.sprite.Sprite):
+    def __init__(self, x, y, w, h):
+        pg.sprite.Sprite.__init__(self)
+        self.image = pg.Surface((w, h))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
