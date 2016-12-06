2c6b0882f3d4a037b2768e26084ff59f3f34d30c
AtheistApe <simiusmathematicus@gmail.com>
2016-12-06 07:14:28 -0800

    Added functions to support sprite animation.

--- Pygame/Platformer/sprites.py
+++ Pygame/Platformer/sprites.py
@@ -12,10 +12,7 @@ class Spritesheet:
         # grab an image out of a larger spritesheet
         image = pg.Surface((width, height))
         image.blit(self.spritesheet, (0, 0), (x, y, width, height))
-
-        # The '//' forces result of division to be an integer.
         image = pg.transform.scale(image, (width//2, height//2))
-
         return image
 
 class Player(pg.sprite.Sprite):
@@ -24,14 +21,38 @@ class Player(pg.sprite.Sprite):
         self.game = game # Player object 'knows' about all objects in game class.
         # self.image = pg.Surface((30, 40))
         # self.image.fill(YELLOW)
-        self.image = self.game.spritesheet.get_image(614, 1063, 120, 191)
-        self.image.set_colorkey(BLACK)
+        self.walking = False
+        self.jumping = False
+        self.current_frame = 0 # Track the animation frame.
+        self.last_update = 0 # To adjust animation speed
+        self.load_images()
+        self.image = self.standing_frames[0]
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
         # self.rect.center = self.pos
         self.rect.midbottom = self.pos
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
+
+    def load_images(self):
+        self.standing_frames = [self.game.spritesheet.get_image(614, 1063, 120, 191),
+                                self.game.spritesheet.get_image(690, 406, 120, 201)]
+        for frame in self.standing_frames:
+            frame.set_colorkey(BLACK)
+
+        self.walk_frames_r = [self.game.spritesheet.get_image(678, 860, 120, 201),
+                              self.game.spritesheet.get_image(692, 1458, 120, 207)]
+        for frame in self.walk_frames_r:
+            frame.set_colorkey(BLACK)
+
+        self.walk_frames_l = []
+        for frame in self.walk_frames_r:
+            frame.set_colorkey(BLACK)
+            self.walk_frames_l.append(pg.transform.flip(frame, True, False))
+
+        self.jump_frame = [self.game.spritesheet.get_image(382, 763, 150, 181)]
+        for frame in self.jump_frame:
+            frame.set_colorkey(BLACK)
 
     def jump(self):
         # Jump only if standing on a platform.
@@ -42,6 +63,7 @@ class Player(pg.sprite.Sprite):
             self.vel.y = -PLAYER_JUMP
 
     def update(self):
+        self.animate()
         self.acc = vec(0, PLAYER_GRAV)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
@@ -64,6 +86,17 @@ class Player(pg.sprite.Sprite):
 
         self.rect.midbottom = self.pos
 
+    def animate(self):
+        now = pg.time.get_ticks()
+        if not self.jumping and not self.walking:
+            if now - self.last_update > 350:
+                self.last_update = now
+                self.current_frame = (self.current_frame + 1) % len(self.standing_frames)
+                bottom = self.rect.bottom
+                self.image = self.standing_frames[self.current_frame]
+                self.rect = self.image.get_rect()
+                self.rect.bottom = bottom
+                
 class Platform(pg.sprite.Sprite):
     def __init__(self, x, y, w, h):
         pg.sprite.Sprite.__init__(self)
